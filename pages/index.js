867398abbbb4e59f76a86e037d1b1a210ffe5d0c
Rafael Luz Gomes <rafaellladm@gmail.com>
2025-12-19 23:23:23 -0300

    Atualização da fraze

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Cristo me deus o soriso mais lindo do mundo! E desse soriso vinheram mais tres!</h1>
+  return <h1>Cristo me deus o soriso mais lindo do mundo, o seu! E desse soriso vinheram mais tres! 🫶😘🫶</h1>
 }
 
 export default Home;
