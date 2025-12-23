2abe9f640efd488c724ca1b92ef40790b0caaddc
Rafael Luz Gomes <rafaellladm@gmail.com>
2025-12-23 14:57:29 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Cristo me deus o soriso mais lindo do mundo, o seu! E desse soriso vinheram mais tres! 🫶😘🫶</h1>
+  return (
+    <h1>
+      Cristo me deus o soriso mais lindo do mundo, o seu! E desse soriso
+      vinheram mais tres! 🫶😘🫶
+    </h1>
+  );
 }
 
 export default Home;
